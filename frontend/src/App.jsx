87b3f436e59a0771fce19387b751af949e954970
yanick <dickbauer.yanick@gmail.com>
2020-05-12 09:29:19 +0200

    frontend added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <ul>
+        <li>
+          blub
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
